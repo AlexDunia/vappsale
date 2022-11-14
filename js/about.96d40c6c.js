94ee57d63e2c04182f63d2bf0a6be053b059745d
Alex Dunia <thealexdunia@gmail.com>
2022-11-14 23:26:15 +0100

    first dist commit

---
+++ js/about.96d40c6c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvappsale"]=self["webpackChunkvappsale"]||[]).push([[443],{7381:function(a,n,e){e.r(n),e.d(n,{default:function(){return o}});var s=e(3396);const u={class:"about"},t=(0,s._)("h1",null,"This is an about page",-1),r=[t];function c(a,n){return(0,s.wg)(),(0,s.iD)("div",u,r)}var i=e(89);const l={},p=(0,i.Z)(l,[["render",c]]);var o=p}}]);
+//# sourceMappingURL=about.96d40c6c.js.map
